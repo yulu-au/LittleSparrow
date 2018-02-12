a1874b054250367ea66e7298795946802b282ee9
luyu <luyu_6666@qq.com>
2018-02-12 21:13:35 +0800

    add 404 act

--- doit.c
+++ doit.c
@@ -3,6 +3,7 @@ void doit(int fd){
 		rio_t rbuffer;
 		char buf[MAXLINE], method[MAXLINE],uri[MAXLINE],version[MAXLINE],filename[MAXLINE],cgi[MAXLINE];
 		int is_static=0;
+		struct stat sbuf;
 		// init the buffer of rio
 		rio_readinit(&rbuffer, fd);
 		//read a line from fd
@@ -12,8 +13,14 @@ void doit(int fd){
 				send_error(fd, "501","Not Implemented");// send what
 				return;
 		}
-		//send_error(fd,"200","ok");
 		is_static = parseuri(uri,filename,cgi);
+		
+		if(stat(filename, &sbuf) < 0){
+			send_error(fd, "404", "Not Found");
+			return;
+		}
+
+
 		if(is_static){// serve static content
 				printf("we would serve static content filename: %s cgi: %s\n",filename,cgi);
 		}
